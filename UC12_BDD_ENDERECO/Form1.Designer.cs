e754ef79c9a02632b47092e8d118892a73a88f00
AsaphGalhardo <asaphgalhardo@gmail.com>
2023-05-22 22:00:40 -0300

    Adicionar arquivos de projeto.

---
+++ UC12_BDD_ENDERECO/Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace UC12_BDD_ENDERECO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNOME = new System.Windows.Forms.TextBox();
+            this.textBoxSOBRENOME = new System.Windows.Forms.TextBox();
+            this.textBoxNOMESOCIAL = new System.Windows.Forms.TextBox();
+            this.textBoxRG = new System.Windows.Forms.TextBox();
+            this.textBoxCPF = new System.Windows.Forms.TextBox();
+            this.radioButtonMASC = new System.Windows.Forms.RadioButton();
+            this.radioButtonFEM = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxLOG = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxBAIRRO = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxCIDADE = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.comboBoxESTADO = new System.Windows.Forms.ComboBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.comboBoxUF = new System.Windows.Forms.ComboBox();
+            this.buttonCADASTRAR = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNOME
+            // 
+            this.textBoxNOME.Location = new System.Drawing.Point(91, 12);
+            this.textBoxNOME.Name = "textBoxNOME";
+            this.textBoxNOME.Size = new System.Drawing.Size(225, 20);
+            this.textBoxNOME.TabIndex = 0;
+            // 
+            // textBoxSOBRENOME
+            // 
+            this.textBoxSOBRENOME.Location = new System.Drawing.Point(91, 64);
+            this.textBoxSOBRENOME.Name = "textBoxSOBRENOME";
+            this.textBoxSOBRENOME.Size = new System.Drawing.Size(225, 20);
+            this.textBoxSOBRENOME.TabIndex = 1;
+            // 
+            // textBoxNOMESOCIAL
+            // 
+            this.textBoxNOMESOCIAL.Location = new System.Drawing.Point(91, 112);
+            this.textBoxNOMESOCIAL.Name = "textBoxNOMESOCIAL";
+            this.textBoxNOMESOCIAL.Size = new System.Drawing.Size(225, 20);
+            this.textBoxNOMESOCIAL.TabIndex = 2;
+            // 
+            // textBoxRG
+            // 
+            this.textBoxRG.Location = new System.Drawing.Point(91, 160);
+            this.textBoxRG.Name = "textBoxRG";
+            this.textBoxRG.Size = new System.Drawing.Size(225, 20);
+            this.textBoxRG.TabIndex = 3;
+            // 
+            // textBoxCPF
+            // 
+            this.textBoxCPF.Location = new System.Drawing.Point(91, 228);
+            this.textBoxCPF.Name = "textBoxCPF";
+            this.textBoxCPF.Size = new System.Drawing.Size(225, 20);
+            this.textBoxCPF.TabIndex = 4;
+            // 
+            // radioButtonMASC
+            // 
+            this.radioButtonMASC.AutoSize = true;
+            this.radioButtonMASC.Location = new System.Drawing.Point(77, 304);
+            this.radioButtonMASC.Name = "radioButtonMASC";
+            this.radioButtonMASC.Size = new System.Drawing.Size(73, 17);
+            this.radioButtonMASC.TabIndex = 5;
+            this.radioButtonMASC.TabStop = true;
+            this.radioButtonMASC.Text = "Masculino";
+            this.radioButtonMASC.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFEM
+            // 
+            this.radioButtonFEM.AutoSize = true;
+            this.radioButtonFEM.Location = new System.Drawing.Point(77, 345);
+            this.radioButtonFEM.Name = "radioButtonFEM";
+            this.radioButtonFEM.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonFEM.TabIndex = 6;
+            this.radioButtonFEM.TabStop = true;
+            this.radioButtonFEM.Text = "Feminino";
+            this.radioButtonFEM.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Sobrenome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nome Social";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Registro Geral";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Cpf";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Etnia";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(46, 418);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(74, 390);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(104, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Data de Nascimento";
+            // 
+            // textBoxLOG
+            // 
+            this.textBoxLOG.Location = new System.Drawing.Point(541, 8);
+            this.textBoxLOG.Name = "textBoxLOG";
+            this.textBoxLOG.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLOG.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(432, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Logradouro";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(438, 67);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Bairro";
+            // 
+            // textBoxBAIRRO
+            // 
+            this.textBoxBAIRRO.Location = new System.Drawing.Point(541, 67);
+            this.textBoxBAIRRO.Name = "textBoxBAIRRO";
+            this.textBoxBAIRRO.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBAIRRO.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(432, 119);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Cidade";
+            // 
+            // textBoxCIDADE
+            // 
+            this.textBoxCIDADE.Location = new System.Drawing.Point(541, 119);
+            this.textBoxCIDADE.Name = "textBoxCIDADE";
+            this.textBoxCIDADE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCIDADE.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(432, 167);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(21, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "UF";
+            // 
+            // comboBoxESTADO
+            // 
+            this.comboBoxESTADO.FormattingEnabled = true;
+            this.comboBoxESTADO.Items.AddRange(new object[] {
+            "Estado\t",
+            "Acre\t",
+            "Alagoas\t",
+            "Amapá\t",
+            "Amazonas\t",
+            "Bahia\t",
+            "Ceará\t",
+            "Espírito Santo\t\t",
+            "Goiás\t\t",
+            "Maranhão\t\t",
+            "Mato Grosso\t\t",
+            "Mato Grosso do Sul\t",
+            "Minas Gerais\t",
+            "Pará\t",
+            "Paraíba\t",
+            "Paraná\t",
+            "Pernambuco\t",
+            "Piauí\t",
+            "Rio de Janeiro\t",
+            "Rio Grande do Norte\t",
+            "Rio Grande do Sul\t",
+            "Rondônia\t",
+            "Roraima\t",
+            "Santa Catarina\t",
+            "São Paulo",
+            "Sergipe\t\t",
+            "Tocantins"});
+            this.comboBoxESTADO.Location = new System.Drawing.Point(520, 228);
+            this.comboBoxESTADO.Name = "comboBoxESTADO";
+            this.comboBoxESTADO.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxESTADO.TabIndex = 23;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(432, 236);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(40, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Estado";
+            // 
+            // comboBoxUF
+            // 
+            this.comboBoxUF.FormattingEnabled = true;
+            this.comboBoxUF.Items.AddRange(new object[] {
+            "Estado\t",
+            "Acre\t",
+            "Alagoas\t",
+            "Amapá\t",
+            "Amazonas\t",
+            "Bahia\t",
+            "Ceará\t",
+            "Espírito Santo\t\t",
+            "Goiás\t\t",
+            "Maranhão\t\t",
+            "Mato Grosso\t\t",
+            "Mato Grosso do Sul\t",
+            "Minas Gerais\t",
+            "Pará\t",
+            "Paraíba\t",
+            "Paraná\t",
+            "Pernambuco\t",
+            "Piauí\t",
+            "Rio de Janeiro\t",
+            "Rio Grande do Norte\t",
+            "Rio Grande do Sul\t",
+            "Rondônia\t",
+            "Roraima\t",
+            "Santa Catarina\t",
+            "São Paulo",
+            "Sergipe\t\t",
+            "Tocantins"});
+            this.comboBoxUF.Location = new System.Drawing.Point(520, 178);
+            this.comboBoxUF.Name = "comboBoxUF";
+            this.comboBoxUF.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxUF.TabIndex = 25;
+            // 
+            // buttonCADASTRAR
+            // 
+            this.buttonCADASTRAR.Location = new System.Drawing.Point(520, 313);
+            this.buttonCADASTRAR.Name = "buttonCADASTRAR";
+            this.buttonCADASTRAR.Size = new System.Drawing.Size(164, 23);
+            this.buttonCADASTRAR.TabIndex = 26;
+            this.buttonCADASTRAR.Text = "Cadastrar";
+            this.buttonCADASTRAR.UseVisualStyleBackColor = true;
+            this.buttonCADASTRAR.Click += new System.EventHandler(this.buttonCADASTRAR_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCADASTRAR);
+            this.Controls.Add(this.comboBoxUF);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.comboBoxESTADO);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBoxCIDADE);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxBAIRRO);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxLOG);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButtonFEM);
+            this.Controls.Add(this.radioButtonMASC);
+            this.Controls.Add(this.textBoxCPF);
+            this.Controls.Add(this.textBoxRG);
+            this.Controls.Add(this.textBoxNOMESOCIAL);
+            this.Controls.Add(this.textBoxSOBRENOME);
+            this.Controls.Add(this.textBoxNOME);
+            this.Name = "Form1";
+            this.Text = "Nome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNOME;
+        private System.Windows.Forms.TextBox textBoxSOBRENOME;
+        private System.Windows.Forms.TextBox textBoxNOMESOCIAL;
+        private System.Windows.Forms.TextBox textBoxRG;
+        private System.Windows.Forms.TextBox textBoxCPF;
+        private System.Windows.Forms.RadioButton radioButtonMASC;
+        private System.Windows.Forms.RadioButton radioButtonFEM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxLOG;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxBAIRRO;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxCIDADE;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox comboBoxESTADO;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox comboBoxUF;
+        private System.Windows.Forms.Button buttonCADASTRAR;
+    }
+}
+
---
+++ UC12_BDD_ENDERECO/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace UC12_BDD_ENDERECO
+{
+    public partial class Form1 : Form
+    {
+        string servidor;
+        MySqlConnection conexao;
+        MySqlCommand comando;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+            servidor = "Server=localhost;Database=pessoa_endereco;Uid=root;Pwd=";
+            conexao = new MySqlConnection(servidor);
+            comando = conexao.CreateCommand();
+        }
+
+        private void buttonCADASTRAR_Click(object sender, EventArgs e)
+        {//insere endereço 
+            try
+            {
+                conexao.Open();
+                comando.CommandText = "INSERT INTO tbl_endereco(logradouro, bairro, estado, cidade, uf) VALUES ('" + textBoxLOG.Text + "', '" + textBoxBAIRRO.Text + "', '" + textBoxCIDADE.Text + "', '" + comboBoxESTADO.Text + "', '" + comboBoxUF.Text + "');";
+                comando.ExecuteNonQuery();
+                MessageBox.Show("cadastrado");
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            //pegar o ultimo id de endereço na tbl 
+
+
+            try
+            {
+                conexao.Open();
+                comando.CommandText = "SELECT MAX(id) FROM tbl_endereco;";
+                MySqlDataReader readerID = comando.ExecuteReader();
+                string ultimoID;
+                if (readerID.Read())
+                {
+                   // ultimoID = readerID.GetString(0);  
+                   ultimoID = 
+                }
+            }
+            catch (Exception erro_mysql)
+            {
+                MessageBox.Show(erro_mysql.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
